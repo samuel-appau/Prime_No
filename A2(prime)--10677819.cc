6ac3445088d22b1d90b104a27a63e7c8263a856b
samuel-appau <42771319+samuel-appau@users.noreply.github.com>
2018-08-29 14:07:34 -0400

    Add files via upload

---
+++ A2(prime)--10677819.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+
+
+int main(){
+
+int num;
+int s=2;
+bool prime=false
+
+std::cout<<"Please input a number";
+std::cin>>num;
+
+for(int s;s<num;s++)
+{
+if((num%s==0){
+prime=true;
+break;
+} 
+if(prime=false){
+std::cout<<"The number is prime";
+else
+std::cout<<"The number is not prime";
+}
+return 0;
+}
+      
+ 
